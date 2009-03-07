87a7e3b2170533a534d659dd779155c38ac63d69
allen.jordan <allen.jordan@54aee591-6e50-0410-9cd2-05afbb262221>
2009-03-07 21:22:52 +0000

    Added a version number, license docs, and the license header comment to each source file.

--- trunk/Franklin Math/src/franklinmath/util/FMProperties.java
+++ trunk/Franklin Math/src/franklinmath/util/FMProperties.java
@@ -1,3 +1,22 @@
+/*
+Copyright 2009 Allen Franklin Jordan (allen.jordan@gmail.com).
+
+This file is part of Franklin Math.
+
+Franklin Math is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Franklin Math is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Franklin Math.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package franklinmath.util;
 
 import java.util.Properties;
@@ -46,12 +65,17 @@ public class FMProperties {
      * Load the default property settings.  
      */
     public static synchronized void LoadDefaults() {
+        SetDouble("program.version", 0.1);
         SetPrecision(34);
         SetRoundingMode(java.math.RoundingMode.HALF_EVEN);
         SetDisplayPrecision(15);
         SetNumPlotPoints(70);
         SetPlotWidth(400);
         SetPlotHeight(300);
+    }
+    
+    public static synchronized double GetProgramVersion() {
+        return GetDouble("program.version");
     }
 
     public static synchronized void SetPrecision(int value) {
